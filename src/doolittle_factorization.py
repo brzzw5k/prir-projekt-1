a1f4f524033b91b5b760db21d10f1ea95634c4d9
Jan Bietkowski <janek2180@gmail.com>
2024-12-25 16:21:30 +0100

    On branch perform_test  Your branch and 'origin/perform_test' have diverged,  and have 1 and 1 different commits each, respectively.
    Changes to be committed:
    	modified:   src/doolittle_factorization.py
    	modified:   tests/test_doolittle_factorization.py
    	modified:   tests/test_performance_doolittle.py

--- src/doolittle_factorization.py
+++ src/doolittle_factorization.py
@@ -5,57 +5,62 @@ class DoolittleFactorization:
     """
     Doolittle's factorization of a square matrix A into
     a lower triangular matrix L and an upper triangular matrix U
-    such that A = LU.
-
-    Parameters:delayed
-        A: A square matrix of shape (n, n)
-
-    Attributes:
-        A: A square matrix of shape (n, n)
-        n: The size of the square matrix A
-        U: An upper triangular matrix of shape (n, n)
-        L: A lower triangular matrix of shape (n, n)
+    such that A = L*U.
     """
 
     def __init__(self, A: np.ndarray) -> None:
         self.A = A
         self.n = A.shape[0]
-        self.U = np.zeros((self.n, self.n))
-        self.L = np.eye(self.n)
+        self.U = np.zeros((self.n, self.n), dtype=A.dtype)
+        self.L = np.eye(self.n, dtype=A.dtype)
 
-    def _compute_U(self, k: int, j: int) -> None:
-        self.U[k, j] = self.A[k, j]
+    def _compute_U_element(self, k: int, j: int) -> None:
+        """
+        U[k,j].
+        """
+        tmp = self.A[k, j]
         for m in range(k):
-            self.U[k, j] -= self.L[k, m] * self.U[m, j]
+            tmp -= self.L[k, m] * self.U[m, j]
+        self.U[k, j] = tmp
 
-    def _compute_L(self, k: int, i: int) -> None:
-        self.L[i, k] = self.A[i, k]
+    def _compute_L_element(self, k: int, i: int) -> None:
+        """
+        L[i,k].
+        """
+        tmp = self.A[i, k]
         for m in range(k):
-            self.L[i, k] -= self.L[i, m] * self.U[m, k]
-        self.L[i, k] /= self.U[k, k]
+            tmp -= self.L[i, m] * self.U[m, k]
+        self.L[i, k] = tmp / self.U[k, k]
 
     @staticmethod
     def sequential(A: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
         df = DoolittleFactorization(A)
-        for k in range(df.n):
-            for j in range(k, df.n):
-                df._compute_U(k, j)
-            for i in range(k + 1, df.n):
-                df._compute_L(k, i)
+        n = df.n
+        for k in range(n):
+            # Obliczenie k-tego wiersza macierzy U
+            for j in range(k, n):
+                df._compute_U_element(k, j)
+            # Obliczenie k-tej kolumny macierzy L
+            for i in range(k + 1, n):
+                df._compute_L_element(k, i)
         return df.L, df.U
 
     @staticmethod
     def parallel(A: np.ndarray, n_jobs: int = -1) -> tuple[np.ndarray, np.ndarray]:
+        """
+        Parallel method on rows and colls
+        """
         df = DoolittleFactorization(A)
-
-        for k in range(df.n):
+        n = df.n
+        for k in range(n):
+            # Równoległe obliczenie k-tego wiersza macierzy U
             Parallel(n_jobs=n_jobs)(
-                delayed(df._compute_U)(k, j)
-                for j in range(k, df.n)
+                delayed(df._compute_U_element)(k, j)
+                for j in range(k, n)
             )
+            # Równoległe obliczenie k-tej kolumny macierzy L
             Parallel(n_jobs=n_jobs)(
-                delayed(df._compute_L)(k, i)
-                for i in range(k + 1, df.n)
+                delayed(df._compute_L_element)(k, i)
+                for i in range(k + 1, n)
             )
-
         return df.L, df.U
--- tests/test_performance_doolittle.py
+++ tests/test_performance_doolittle.py
@@ -2,9 +2,8 @@ import numpy as np
 import pytest
 from src.doolittle_factorization import DoolittleFactorization
 
-
 @pytest.mark.benchmark(group="doolittle_factorization_seq")
-@pytest.mark.parametrize("matrix_size", [3, 10, 25, 100])
+@pytest.mark.parametrize("matrix_size", [200])
 def test_doolittle_factorization_sequential_performance(benchmark, matrix_size):
     """
     Sequential version execution time measurement
@@ -18,16 +17,19 @@ def test_doolittle_factorization_sequential_performance(benchmark, matrix_size):
     benchmark(doolittle_sequential)
 
 
-@pytest.mark.benchmark(group="doolittle_factorization_par")
-@pytest.mark.parametrize("matrix_size", [3, 10, 25, 100])
-def test_doolittle_factorization_parallel_performance(benchmark, matrix_size):
+@pytest.mark.benchmark(group="doolittle_factorization_parallel_rows_cols")
+@pytest.mark.parametrize("matrix_size", [200])
+@pytest.mark.parametrize("n_jobs", [4])
+def test_doolittle_factorization_parallel(benchmark, matrix_size, n_jobs):
     """
-    Parallel version execution time measurement
+    Parallel version (rows & cols) execution time measurement
+    with different n_jobs (number of cores).
     """
     np.random.seed(42)
     A = np.random.rand(matrix_size, matrix_size)
 
     def doolittle_parallel():
-        DoolittleFactorization.parallel(A, n_jobs=4)
+        DoolittleFactorization.parallel(A, n_jobs=n_jobs)
 
     benchmark(doolittle_parallel)
+
