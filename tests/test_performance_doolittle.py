fcf36a118a6d6f6e4ac79ef1ea0a32a7f7bb56ba
Jan Bietkowski <janek2180@gmail.com>
2025-01-01 22:58:28 +0100

    modified:   test_performance_doolittle.py

--- tests/test_performance_doolittle.py
+++ tests/test_performance_doolittle.py
@@ -46,21 +46,3 @@ def test_doolittle_factorization_parallel_threads_performance(
         return DoolittleFactorization.parallel_threads(A, n_threads)
 
     benchmark(doolittle_parallel_threads)
-
-
-@pytest.mark.skip(reason="Skipping parallel performance test on CI")
-@pytest.mark.benchmark(group="doolittle_factorization_parallel_numba")
-@pytest.mark.parametrize("matrix_size", [7000])
-@pytest.mark.parametrize("n_threads", [2, 4, 6, 8, 10, 12, 14, 16])
-def test_doolittle_factorization_parallel_numba_performance_more_threads(
-    benchmark, matrix_size, n_threads
-):
-    np.random.seed(42)
-    A = np.random.rand(matrix_size, matrix_size).astype(np.float64)
-
-    numba.set_num_threads(n_threads)
-
-    def doolittle_parallel_numba():
-        return DoolittleFactorization.parallel_numba(A)
-
-    benchmark(doolittle_parallel_numba)
